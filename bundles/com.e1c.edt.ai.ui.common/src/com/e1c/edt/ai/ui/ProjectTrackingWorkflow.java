255f2ab1723b83b7a327189ec881212a2c7e18c3
Nikolay Pianikov <nikolayp@mail.ru>
2025-06-06 11:05:38 +0300

    IDEAI-287 Global context sync issues - Documents may occasionally fail to update

--- bundles/com.e1c.edt.ai.ui.common/src/com/e1c/edt/ai/ui/ProjectTrackingWorkflow.java
+++ bundles/com.e1c.edt.ai.ui.common/src/com/e1c/edt/ai/ui/ProjectTrackingWorkflow.java
@@ -365,23 +365,27 @@ class ProjectTrackingWorkflow
             filesToSync.remove(file);
             if (file.aiCtx.getDocument() != null)
             {
+                var documentUpdates = new ArrayList<GlobalContextUpdate>();
+                documentUpdates.add(update);
                 features.add(
-                    globalContextSync.syncUpdates(file.aiCtx, initialStateSent, filesUpdates, 5,
+                    globalContextSync.syncUpdates(file.aiCtx, initialStateSent, documentUpdates, 5,
                         statisticsProvider.get(), cancellationToken));
-
+                initialStateSent = true;
                 continue;
             }
 
             filesUpdates.add(update);
         }
 
-        features.add(
-            globalContextSync.syncUpdates(new AIContext(projectId, "", null), initialStateSent, filesUpdates, 5, //$NON-NLS-1$
-                statisticsProvider.get(), cancellationToken));
+        if (!filesUpdates.isEmpty())
+        {
+            features.add(
+                globalContextSync.syncUpdates(new AIContext(projectId, "", null), initialStateSent, filesUpdates, 5, //$NON-NLS-1$
+                    statisticsProvider.get(), cancellationToken));
+            initialStateSent = true;
+        }
 
         CompletableFuture.allOf(features.toArray(new CompletableFuture[features.size()])).join();
-
-        initialStateSent = true;
         if (!filesToSync.isEmpty())
         {
             return new Result(ProjectTrackingWorkflowState.SYNC, ShortDelay);
