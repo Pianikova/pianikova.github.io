743b6ed9e95fe06dac2a6377a8b9f170da8d1a79
Nikolay Pianikov <nikolayp@mail.ru>
2025-09-01 15:21:00 +0300

    IDEAI-343 "No head" exception on first git commit when generating commit message using AI

--- bundles/com.e1c.edt.ai.ui.common/src/com/e1c/edt/ai/ui/GitTools.java
+++ bundles/com.e1c.edt.ai.ui.common/src/com/e1c/edt/ai/ui/GitTools.java
@@ -5,12 +5,16 @@ package com.e1c.edt.ai.ui;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.dircache.DirCacheIterator;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.treewalk.EmptyTreeIterator;
 
 public class GitTools implements IGitTools
 {
@@ -18,14 +22,33 @@ public class GitTools implements IGitTools
     public void getDiff(Repository repository, int contextLines, OutputStream gitDiffStream)
         throws GitAPIException, IOException
     {
-        try (var git = new Git(repository); var diffFormatter = new DiffFormatter(gitDiffStream))
+        try (Git git = new Git(repository); DiffFormatter diffFormatter = new DiffFormatter(gitDiffStream))
         {
             diffFormatter.setRepository(repository);
             diffFormatter.setContext(contextLines);
-            var diffs = git.diff().setCached(true).setShowNameAndStatusOnly(false).call();
-            for (DiffEntry entry : diffs)
+            // Check for commits in the repository
+            var headCommitId = repository.resolve(Constants.HEAD);
+            List<DiffEntry> diffs;
+            if (headCommitId == null)
             {
-                diffFormatter.format(entry);
+                // Empty repository case: compare empty tree with index
+                try (var reader = repository.newObjectReader())
+                {
+                    var headTreeIter = new EmptyTreeIterator();
+                    var indexTreeIter = new DirCacheIterator(repository.readDirCache());
+                    diffs = diffFormatter.scan(headTreeIter, indexTreeIter);
+                }
+            }
+            else
+            {
+                // Normal case: compare HEAD with index
+                diffs = git.diff().setCached(true).setShowNameAndStatusOnly(false).call();
+            }
+
+            // Format all found differences
+            for (var diff : diffs)
+            {
+                diffFormatter.format(diff);
             }
         }
     }
