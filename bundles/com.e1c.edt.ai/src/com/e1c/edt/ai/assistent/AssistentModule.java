d3b26f25cfc5c2a41d3f2170d26f6027d2464b54
Nikolay Pianikov <nikolayp@mail.ru>
2025-10-07 15:59:48 +0300

    IDEAI-382 Handle expiration so the user doesn't see an error - remove conversations

--- bundles/com.e1c.edt.ai/src/com/e1c/edt/ai/assistent/AssistentModule.java
+++ bundles/com.e1c.edt.ai/src/com/e1c/edt/ai/assistent/AssistentModule.java
@@ -33,7 +33,6 @@ public class AssistentModule
         bind(IThreadManager.class).to(ThreadManager.class).in(Singleton.class);
         bind(IGlobalContextService.class).to(GlobalContextService.class).in(Singleton.class);
         bind(ICompressor.class).to(Compressor.class).in(Singleton.class);
-        bind(IConversations.class).to(Conversations.class).in(Singleton.class);
         bind(ITools.class).to(Tools.class).in(Singleton.class);
         // @formatter:on
     }
